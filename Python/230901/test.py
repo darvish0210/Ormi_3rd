1efa1919f24c41590405053c9f7540ef8343dda9
darvish0210 <142385778+darvish0210@users.noreply.github.com>
2023-09-01 17:59:52 +0900

    Add files via upload

---
+++ Python/230901/test.py
@@ -0,0 +1,11 @@
+def qsort(arr):
+    if len(arr) <=1:
+        return arr
+    pivot = arr[len(arr)//2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return qsort(left) + middle + qsort(right)
+
+xs = qsort([3,6,8,10,1,2,1])
+print(xs)
