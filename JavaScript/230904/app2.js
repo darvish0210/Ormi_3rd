755eeb79ec6f1bb22ef42abe18e4af1784ae8469
Yoonjae <ksmonesia@gmail.com>
2023-09-06 04:04:44 +0900

    firstcommit

---
+++ JavaScript/230904/app2.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.innerHTML = 'Press me';
+    button.className = 'btn-styled';
+
+    button.onclick = function() {
+        // …
+        
+    };
+
+    var container = document.getElementById('container');
+    container.appendChild(button);
+}, false);
